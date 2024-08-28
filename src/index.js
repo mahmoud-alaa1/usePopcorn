7dc19df82322acd79fa150626edb701a565067b7
Mahmoud Alaa <asticalaa1@gmail.com>
2024-08-29 02:50:24 +0300

    Star Rating component is now customizable

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating  />
+    <StarRating
+      maxRating={5}
+      color="#fc1235"
+      size={50}
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={5}
+    />
+    <StarRating />
   </React.StrictMode>
 );
